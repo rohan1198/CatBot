7bacbcfedd6fc901541ddbcccf29884d0fcfb87a
rohan1198 <rohan1198@gmail.com>
2024-01-06 17:37:11 +0000

    convert model weights to onnx

--- utils/convert_to_onnx.py
+++ utils/convert_to_onnx.py
@@ -3,8 +3,13 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, required=True, help="Model name or path to the model")
-    parser.add_argument("--format", type=str, default="onnx", help="Expected export format")
+    parser.add_argument("--model", type=str, required=True,
+                        help="Model name or path to the model")
+    parser.add_argument(
+        "--format",
+        type=str,
+        default="onnx",
+        help="Expected export format")
     args = parser.parse_args()
 
     model = YOLO(args.model)
