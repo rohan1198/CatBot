016b9a1f667c0d793144bd906298e9cce073522d
rohan1198 <rohan1198@gmail.com>
2024-03-09 09:18:13 +0000

    modified sleep time

--- cat_detector.sh
+++ cat_detector.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-# Configuration
-MAX_RETRIES=3
-RETRY_COUNT=0
-SCRIPT_PATH="/home/raspi/CatBot/main.py"
-LOG_FILE="/home/raspi/CatBot/logfile.log"
-VENV_PATH="/home/raspi/venvs/yolocat/bin/activate"
+[Unit]
+Description=Cat Detection system
+After=network.target
 
-echo "Starting script monitoring" >> "$LOG_FILE"
+[Service]
+Type=forking
+ExecStart=/bin/bash /home/raspi/CatBot/cat_detector.sh start
+ExecStop=/bin/bash /home/raspi/CatBot/cat_detector.sh stop
+Restart=on-failure
+RestartSec=5
 
-while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
-    if pgrep -f $SCRIPT_PATH > /dev/null
-    then
-        echo "$(date): Script is running." >> "$LOG_FILE"
-    else
-        echo "$(date): Script not running, starting script." >> "$LOG_FILE"
-        # Activate the virtual environment
-        source $VENV_PATH
-        # Run the script in the virtual environment
-        python3 $SCRIPT_PATH &
-        RETRY_COUNT=$((RETRY_COUNT+1))
-    fi
-    sleep 60  # Check every 60 seconds
-done
-
-echo "$(date): Maximum retries reached, rebooting system." >> "$LOG_FILE"
-sudo reboot
+[Install]
+WantedBy=multi-user.target
